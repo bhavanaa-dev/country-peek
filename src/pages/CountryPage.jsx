4e22800b8008a5954229ae3f50e7cd1a63a14376
Bhavana <bhavanaaass@gmail.com>
2026-08-19 17:01:18 +0530

    feat: scaffold CountryPeek with routing, Header, and Home shell

---
+++ src/pages/CountryPage.jsx
@@ -0,0 +1,10 @@
+function CountryPage() {
+  return (
+    <div className="page-placeholder">
+      <h2>Country Details</h2>
+      <p>Country details will be added in a later part.</p>
+    </div>
+  );
+}
+
+export default CountryPage;
---
+++ src/pages/Favourites.jsx
@@ -0,0 +1,10 @@
+function Favourites() {
+  return (
+    <div className="page-placeholder">
+      <h2>Favourites</h2>
+      <p>Your favourite countries will appear here.</p>
+    </div>
+  );
+}
+
+export default Favourites;
